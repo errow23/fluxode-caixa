b634b46123d7bb3154c75a0ba3ad2f7eecdc82e2
errow23 <eronfelixd@gmail.com>
2024-07-09 10:09:48 -0300

    fluxodecaixa

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const recebrv= document.querySelector(".button")
+
+function reValues(){
+    const produtos= document.querySelector(".Produtos").value
+    const Valor= document.querySelector(".Valor").value
+    const forma= document.querySelector(".Forma").value
+    const Vendedora= document.querySelector(".Vendedora").value
+    
+console.log(produtos, Valor,forma,Vendedora);
+}
+
+
+recebrv,addEventListener("click", reValues)
+
+
+
+
+
+
